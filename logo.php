b269ea2c53a98e2762bd56701b92763adfde24e3
Chandrakant <aryalinux11@gmail.com>
2019-10-19 00:17:12 +0530

    Changed default font to titilium web

--- logo.php
+++ logo.php
@@ -1,5 +1,3 @@
 <div class="container">
-    <div style="padding: 2em;">
-        <div class="heading" style="font-size: 2em; "><span style="color: #ff0000">arya</span>linux</div>
-    </div>
+    <div class="heading"><span style="color: #ff0000">Arya</span>Linux</div>
 </div>
