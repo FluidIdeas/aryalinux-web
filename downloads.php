de814a4bafbe9a742e175fe685460218fde0d2d2
Chandrakant Singh <chandrakant.singh@tavant.com>
2019-10-18 15:21:31 +0530

    Updated downloads.php and style.css

--- downloads.php
+++ downloads.php
@@ -8,11 +8,75 @@
   <meta name="description" content="AryaLinux, Linux built from source">
   <meta name="author" content="Chandrakant">
   <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
 <body>
     <?php include_once('logo.php')?>
     <?php include_once('main-menu.php')?>
+    <div class="container">
+      <h1>Downloads</h1>
+      <h3>Latest Version</h3>
+      <table class="fluid-table" cellspacing="0">
+        <thead>
+          <tr>
+            <th>Download URL</th>
+            <th>Checksum</th>
+            <th>Size</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td><a href="#">aryalinux-gnome-x86_64-2.0.iso</a></td>
+            <td>ec6507b8de234bedcebe5ab43e9a26ef</td>
+            <td>2.1 GB</td>
+          </tr>
+          <tr>
+            <td><a href="#">aryalinux-xfce-x86_64-2.0.iso</a></td>
+            <td>ec6507b8de234bedcebe5ab43e9a26ef</td>
+            <td>2.1 GB</td>
+          </tr>
+        </tbody>
+      </table>
+      <h3>Older Version</h3>
+      <p>Older versions of AryaLinux can be downloaded from our sourceforge home. Click <a href="#">here</a> to view all releases.</p>
+      <h3>Other Downloads</h3>
+      <p>AryaLinux is not just another distribution but also provides a platform for building your own GNU/Linux system from source code.
+      Listed below are files that provide support to builders who wish to build their own GNU/Linux Operating System based on AryaLinux.
+      </p>
+      <table class="fluid-table" cellspacing="0">
+        <thead>
+          <tr>
+            <th>Download</th>
+            <th>Checksum</th>
+            <th>Size</th>
+            <th>Documentation</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td><a href="#">Base System binary tarball</a></td>
+            <td>ec6507b8de234bedcebe5ab43e9a26ef</td>
+            <td>280 MB</td>
+            <td><a href="#">Documentation</a></td>
+          </tr>
+          <tr>
+            <td><a href="#">Base System with X Server binary tarball</a></td>
+            <td>ec6507b8de234bedcebe5ab43e9a26ef</td>
+            <td>700 MB</td>
+            <td><a href="#">Documentation</a></td>
+          </tr>
+          <tr>
+            <td><a href="#">Base System with Gnome binary tarball</a></td>
+            <td>ec6507b8de234bedcebe5ab43e9a26ef</td>
+            <td>2 GB</td>
+            <td><a href="#">Documentation</a></td>
+          </tr>
+        </tbody>
+      </table>
+      <br>
+      <br>
+    </div>
     <?php include_once('footer.php')?>
 </body>
 </html>
