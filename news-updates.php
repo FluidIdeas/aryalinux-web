c8fec3df1cc15f86a985cac3e6cded9548796922
Chandrakant Singh <aryalinux11@gmail.com>
2020-01-09 10:43:29 +0530

    Updated the news snippets file

--- news-updates.php
+++ news-updates.php
@@ -1,4 +1,5 @@
 <ul>
+<li>January 09, 2020 - AryaLinux 2.1 Released.</li>
 <li>December 30, 2019 - First KDE Plasma build successful. The latest release would ship with the KDE plasma desktop ISO.</li>
 <li>December 30, 2019 - Starting work on the new website for the new domain.</li>
 <li>December 27, 2019 - Started working on version 2.1, the next release based on LFS stable 9.0.</li>
